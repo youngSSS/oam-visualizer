4bbdda9ff201ab1109d772dae6b5f3bd1266fcfa
YoungSSS <young4127s@naver.com>
2021-08-02 20:19:56 +0900

    app.yaml parsing

--- oam_yaml_parser.py
+++ oam_yaml_parser.py
@@ -1,22 +1,54 @@
 import yaml
+import pprint
 
+COMPONENT_KEYWORDS = ["type", "properties", "traits"]
 
-oam_yaml_path = "./yaml_samples/1.Helloworld/app.yaml"
+yaml_path = [
+    "./tests/2.ServiceTracker_App/KubeVelaManifest/app.yaml",
+    "./tests/2.ServiceTracker_App/KubeVelaManifest/enhanced-webservice.yaml",
+]
+application = {"name": {}, "components": {}}
+yaml_data = {}
+
 
 # This function returns oam-yaml file as dict type data
-def read_oam_yaml():
-    global oam_yaml_path
+def read_yaml():
+    global yaml_path
+    yaml_list = []
 
-    with open(oam_yaml_path) as file:
-        oam_yaml = yaml.load(file, Loader=yaml.FullLoader)
+    for path in yaml_path:
+        with open(path) as file:
+            yaml_list.append(yaml.load(file, Loader=yaml.FullLoader))
 
-    return oam_yaml
+    return yaml_list
 
 
-def parse_oam_yaml(oam_yaml):
-    print(type(oam_yaml))
+def parse_yaml(yaml_list):
+    global application, yaml_data, COMPONENT_KEYWORDS
+
+    for yaml in yaml_list:
+        # CASE: 주진입점이 되는 YAML
+        if yaml["kind"] == "Application":
+            application["name"] = yaml["metadata"]["name"]
+            for component in yaml["spec"]["components"]:
+                application["components"][component["name"]] = {}
+                for keyword in COMPONENT_KEYWORDS:
+                    if keyword in component:
+                        application["components"][component["name"]][keyword] = component[keyword]
+
+        # CASE: Application에서 사용하는 YAML
+        else:
+            yaml_data[yaml["metadata"]["name"]] = {"kind": yaml["kind"]}
+
+
+def print_data():
+    pp = pprint.PrettyPrinter(width=20, indent=4)
+    pp.pprint(application)
+    print()
+    pp.pprint(yaml_data)
 
 
 if __name__ == "__main__":
-    oam_yaml = read_oam_yaml()
-    parse_oam_yaml(oam_yaml)
+    yaml_list = read_yaml()
+    parse_yaml(yaml_list)
+    print_data()
