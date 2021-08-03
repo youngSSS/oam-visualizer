fa7eda260cc4504e9d90c5abac56aedef05517b8
YoungSSS <young4127s@naver.com>
2021-08-03 16:07:40 +0900

    parsing 완료

--- oam_yaml_parser.py
+++ oam_yaml_parser.py
@@ -1,14 +1,24 @@
 import yaml
 import pprint
 
-COMPONENT_KEYWORDS = ["type", "properties", "traits"]
-
 yaml_path = [
     "./tests/2.ServiceTracker_App/KubeVelaManifest/app.yaml",
     "./tests/2.ServiceTracker_App/KubeVelaManifest/enhanced-webservice.yaml",
 ]
-application = {"name": {}, "components": {}}
-yaml_data = {}
+
+# yaml_path = [
+#     "./tests/3.BikeSharing360_MultiContainer_App/KubeVelaManifest/app.yaml",
+#     "./tests/3.BikeSharing360_MultiContainer_App/KubeVelaManifest/enhanced-webservice.yaml",
+# ]
+
+apps = []
+components = {}
+traits = []
+workloads = []
+
+app_component = {}
+component_trait = {}
+component_workload = {}
 
 
 # This function returns oam-yaml file as dict type data
@@ -24,28 +34,91 @@ def read_yaml():
 
 
 def parse_yaml(yaml_list):
-    global application, yaml_data, COMPONENT_KEYWORDS
+    global apps, components, traits, workloads, app_component, component_trait, component_workload
 
     for yaml in yaml_list:
-        # CASE: 주진입점이 되는 YAML
-        if yaml["kind"] == "Application":
-            application["name"] = yaml["metadata"]["name"]
-            for component in yaml["spec"]["components"]:
-                application["components"][component["name"]] = {}
-                for keyword in COMPONENT_KEYWORDS:
-                    if keyword in component:
-                        application["components"][component["name"]][keyword] = component[keyword]
 
-        # CASE: Application에서 사용하는 YAML
+        # CASE: Application YAML
+        if yaml["kind"] == "Application":
+            app_name = yaml["metadata"]["name"]
+
+            if app_name not in apps:
+                apps.append(app_name)
+
+                if app_name not in app_component:
+                    app_component[app_name] = []
+
+            for component in yaml["spec"]["components"]:
+                component_name = component["name"]
+                component_type = component["type"]
+
+                if component_type not in components:
+                    components[component_type] = []
+                    component_trait[component_type] = []
+
+                if component_name not in components[component_type]:
+                    components[component_type].append(component_name)
+
+                if component_name not in app_component[app_name]:
+                    app_component[app_name].append(component_name)
+
+                if "traits" in component:
+                    for trait in component["traits"]:
+                        trait_type = trait["type"]
+
+                        if trait_type not in traits:
+                            traits.append(trait_type)
+
+                        if trait_type not in component_trait[component_type]:
+                            component_trait[component_type].append(trait_type)
+
+        # CASE: Component YAML
+        elif yaml["kind"] == "ComponentDefinition":
+            component_type = yaml["metadata"]["name"]
+
+            if component_type not in component_workload:
+                component_workload[component_type] = []
+
+            if "workload" in yaml["spec"]:
+                for workload in yaml["spec"]["workload"].values():
+                    workload_name = workload["kind"]
+
+                    if workload_name not in workloads:
+                        workloads.append(workload_name)
+
+                    if workload_name not in component_workload[component_type]:
+                        component_workload[component_type].append(workload_name)
+
+        # CASE: Exception
         else:
-            yaml_data[yaml["metadata"]["name"]] = {"kind": yaml["kind"]}
+            print("================== New Type: " + yaml["kind"] + " ==================")
 
 
 def print_data():
+    global apps, components, traits, workloads, app_component, component_trait, component_workload
+
     pp = pprint.PrettyPrinter(width=20, indent=4)
-    pp.pprint(application)
+
+    pp.pprint("Apps")
+    pp.pprint(apps)
     print()
-    pp.pprint(yaml_data)
+    pp.pprint("Components")
+    pp.pprint(components)
+    print()
+    pp.pprint("Traits")
+    pp.pprint(traits)
+    print()
+    pp.pprint("Workloads")
+    pp.pprint(workloads)
+    print()
+    pp.pprint("App-Component")
+    pp.pprint(app_component)
+    print()
+    pp.pprint("Component-Trait")
+    pp.pprint(component_trait)
+    print()
+    pp.pprint("Component-Workload")
+    pp.pprint(component_workload)
 
 
 if __name__ == "__main__":
