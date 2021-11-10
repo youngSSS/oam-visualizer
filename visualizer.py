aa37b49954020f7f1572b45703bcc035ed10ca93
YoungSSS <young4127s@naver.com>
2021-11-10 09:29:57 +0900

    Visualizer

---
+++ visualizer.py
@@ -0,0 +1,132 @@
+import os
+import threading
+from enum import Enum
+from time import sleep
+from threading import Thread
+
+# Constant
+SLEEP_TERM = 60
+
+# Global variable
+USER_INPUT = ""
+PARSED_APP_DATA = dict()
+PARSED_APP_DATA_LOCK = threading.Lock()
+
+
+class C(Enum):
+    COMPONENT = 0
+    TYPE = 1
+    TRAITS = 2
+    PHASE = 3
+    HEALTHY = 4
+    STATUS = 5
+    CREATED_TIME = 6
+    CHILD = 7
+
+
+def print_usage():
+    print()
+    print("------------------ How To Use ------------------\n")
+    print(">> s          - Show application list with app_id")
+    print(">> v app_name - Visualize application status")
+    print(">> q          - Terminate the program")
+    print()
+
+
+def update_app_data():
+    PARSED_APP_DATA_LOCK.acquire()
+
+    data = os.popen("vela ls").read().split("\n")
+
+    parent_component = ""
+    for i in range(1, len(data) - 1):
+
+        # Separate words
+        item = data[i].split("\t")
+        for j in range(len(item)):
+            item[j] = item[j].strip()
+
+        # Case: Child component
+        if item[0] == "├─" or item[0] == "└─":
+            PARSED_APP_DATA[parent_component].append(item[1:])
+        # Case: Parent component
+        else:
+            PARSED_APP_DATA[item[0]] = item[1:]
+            parent_component = item[0]
+
+    print(PARSED_APP_DATA)
+
+    PARSED_APP_DATA_LOCK.release()
+
+
+def print_app_list():
+    global PARSED_APP_DATA, PARSED_APP_DATA_LOCK
+    print("print_app_list")
+
+    update_app_data()
+
+    PARSED_APP_DATA_LOCK.acquire()
+
+    for key in PARSED_APP_DATA:
+        print(key)
+
+    PARSED_APP_DATA_LOCK.release()
+
+
+def visualize_app_data(user_input):
+    global PARSED_APP_DATA, PARSED_APP_DATA_LOCK
+
+    app_name = user_input[2:]
+
+    if app_name == "" or user_input[1] != " ":
+        print("Command format error, please follow below usage")
+        print_usage()
+        return
+
+    update_app_data()
+
+    PARSED_APP_DATA_LOCK.acquire()
+
+    PARSED_APP_DATA_LOCK.release()
+
+
+# Handle user input
+def thread2_routine():
+    global USER_INPUT, PARSED_APP_DATA, PARSED_APP_DATA_LOCK
+
+    while True:
+        USER_INPUT = input()
+
+        if USER_INPUT == "q":
+            break
+
+        elif USER_INPUT == "s":
+            print_app_list()
+
+        elif USER_INPUT[0] == "v":
+            visualize_app_data(USER_INPUT)
+
+        else:
+            print("Error, unknown command")
+
+
+# Update the application information every SLEEP_TERM
+def thread1_routine():
+    while True:
+        update_app_data()
+        sleep(SLEEP_TERM)
+
+
+if __name__ == "__main__":
+    print_usage()
+
+    thread1 = Thread(target=thread1_routine)
+    thread2 = Thread(target=thread2_routine)
+
+    thread1.daemon = True
+    thread2.daemon = True
+
+    thread1.start()
+    thread2.start()
+
+    thread2.join()
